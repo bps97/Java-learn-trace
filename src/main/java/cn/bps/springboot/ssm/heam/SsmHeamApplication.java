1706666fec684fe6fb317fc55a67ce92d7a695d2
bps97 <sina6213czy@163.com>
2020-03-30 17:07:26 +0800

    20200330-初始化sprintboot项目

---
+++ src/main/java/cn/bps/springboot/ssm/heam/SsmHeamApplication.java
@@ -0,0 +1,13 @@
+package cn.bps.springboot.ssm.heam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmHeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmHeamApplication.class, args);
+    }
+
+}
